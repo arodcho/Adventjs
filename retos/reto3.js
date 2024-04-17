8bc8430912af14c0ef17f4cdd01b73156a81fc14
arc0027 <104079189+arc0027@users.noreply.github.com>
2024-04-17 13:06:22 +0200

    Cambios reto 3

--- retos/reto3.js
+++ retos/reto3.js
@@ -1,11 +1,11 @@
-function findNaughtyStep(original, modified) {
-    if (original.length === modified.length) return ""
-      const count = Math.max(original.length, modified.length)
-    
-    for (let i = 0; i < count; i++) {
-            if (original[i] !== modified[i]) {
-                return modified.length > original.length ? modified[i] : original[i]
-        }
-    }
+function findNaughtyStep(original, modified) { 
+    if (original.length === modified.length) return ""; 
+    const count = Math.max(original.length, modified.length); 
+    const currentElement = modified.length > original.length ? modified : original; 
+    for (let i = 0; i < count; i++) { 
+        if (original[i] !== modified[i]) { 
+            return currentElement[i];
+        } 
+    } 
 }
     
