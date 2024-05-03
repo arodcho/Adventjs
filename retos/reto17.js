5e9d36f0c497e0cb1bdfe57e482b297dd9983f65
arc0027 <104079189+arc0027@users.noreply.github.com>
2024-05-03 13:23:06 +0200

    Reto #17: 🛷 Optimizando el alquiler

---
+++ retos/reto17.js
@@ -0,0 +1,17 @@
+function optimizeIntervals(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    const result = [intervals[0]];
+
+    for (const currentInterval of intervals) {
+        const lastMergedInterval = result[result.length - 1];
+
+        if (currentInterval[0] <= lastMergedInterval[1]) {
+            lastMergedInterval[1] = Math.max(lastMergedInterval[1], currentInterval[1]);
+        } else {
+            result.push(currentInterval);
+        }
+    }
+
+    return result;
+}
