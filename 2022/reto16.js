5539e25f5456ec193165c1bd8d79de9f6edbcd1c
arc0027 <104079189+arc0027@users.noreply.github.com>
2024-05-28 14:01:27 +0200

    Reto #16: Arreglando las cartas de Papá Noel

---
+++ 2022/reto16.js
@@ -0,0 +1,10 @@
+function fixLetter(letter) {
+    return letter
+      .trim()
+      .replace(/([.,!])(.*)/g, '$1 $2')
+      .replace(/([\s.,!?])(?=\1)/g, '')
+      .replace(/\s+([.,!?])/g, '$1')
+      .replace(/\b([.?!] \w)|(^\w)/g, str => str.toUpperCase())
+      .replace(/^.*\w$/, str => `${str}.`)
+      .replace(/santa claus/gi, 'Santa Claus');
+}
