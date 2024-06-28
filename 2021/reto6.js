daa9cf0561213e852c08082bf4dd3b3280c50346
arc0027 <104079189+arc0027@users.noreply.github.com>
2024-06-28 10:46:12 +0200

    Reto #6: Rematando los exámenes finales

---
+++ 2021/reto6.js
@@ -0,0 +1,20 @@
+export default function sumPairs(numbers, result) {
+    const set = new Set(numbers)
+  
+    const isRepeatedNumber = ( number) =>
+      numbers.filter(value => number === value).length > 1
+  
+    for (const number1 of set) {
+      const number2 = result - number1
+  
+      if (number1 === number2) {
+        if (isRepeatedNumber(number1)) {
+          return [number1, number1]
+        }
+      } else if (set.has(number2)) {
+        return [number1, number2]
+      }
+    }
+  
+    return null
+  }
