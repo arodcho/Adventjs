8f7732cdda60b0fab61da236bb1d0f8c6bd37a77
arc0027 <104079189+arc0027@users.noreply.github.com>
2024-07-02 09:01:26 +0200

    Reto #10: La máquina de cambio

---
+++ 2021/reto10.js
@@ -0,0 +1,15 @@
+export default function getCoins(change) {
+    const coins = [50, 20, 10, 5, 2, 1];
+
+    return coins
+        .reduce((acc, coin, i) => {
+            if (change >= coin) {
+                const nCoins = Math.floor(change / coin);
+                acc[i] = nCoins;
+                change -= coin * nCoins;
+            }
+
+            return acc;
+        }, [0, 0, 0, 0, 0, 0])
+        .reverse();
+}
